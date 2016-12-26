d5fef977927d230c56d05cbbf031a8d42b7e2a8e
Michelle Tilley <binarymuse@github.com>
2016-12-25 20:36:25 -0800

    Add crossfade to menu pages

--- src/renderer/components/menu.js
+++ src/renderer/components/menu.js
@@ -1,31 +1,106 @@
 import React from 'react'
+import { TransitionMotion, spring } from 'react-motion'
 import { connect } from 'react-redux'
 import { autobind } from 'core-decorators'
 
 import WifiInfo from './wifi-info'
+
+const PAGES = {
+  DEFAULT: "DEFAULT",
+  WIFI: "WIFI",
+  IMAGES: "IMAGES"
+}
 
 class Menu extends React.Component {
   static defaultProps = {
     onClose: () => {}
   }
 
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      page: PAGES.DEFAULT
+    }
+  }
+
+  willEnter () {
+    return {opacity: -1}
+  }
+
+  willLeave () {
+    return {opacity: spring(-1)}
+  }
+
   render () {
     return (
-      <div className='full-screen menu' style={this.props.style}>
-        <div className='close-button' onClick={this.handleClose}>
-          <img src='./img/icon-close.png' />
-        </div>
+      <TransitionMotion
+        willEnter={this.willEnter}
+        willLeave={this.willLeave}
+        styles={
+          [{
+            key: this.state.page,
+            data: this.renderPage(this.state.page),
+            style: {opacity: spring(1)}
+          }]
+        }
+      >
+        {interpolatedStyles => {
+          return (
+            <div className='full-screen menu' style={this.props.style}>
+              {interpolatedStyles.map(({key, data, style}) => (
+                React.cloneElement(data, {style: {opacity: Math.max(style.opacity, 0)}})
+              ))}
 
-        <div className='image-button'>
+              <div className='close-button' onClick={this.handleClose}>
+                <img src='./img/icon-close.png' />
+              </div>
+
+              <WifiInfo />
+            </div>
+          )
+        }}
+      </TransitionMotion>
+    )
+  }
+
+  renderPage (page) {
+    switch (page) {
+      case PAGES.WIFI:
+        return this.renderWifi()
+      case PAGES.IMAGES:
+        return this.renderImages()
+      default:
+        return this.renderDefault()
+    }
+  }
+
+  renderDefault () {
+    return (
+      <div className='full-screen-floating' key='default'>
+        <div className='image-button' onClick={() => this.changePage(PAGES.WIFI)}>
           <img src='./img/icon-wifi.png' />
           <span>Wifi</span>
         </div>
-        <div className='image-button'>
+        <div className='image-button' onClick={() => this.changePage(PAGES.IMAGES)}>
           <img src='./img/icon-gallery.png' />
           <span>Images</span>
         </div>
+      </div>
+    )
+  }
 
-        <WifiInfo />
+  renderWifi () {
+    return (
+      <div className='full-screen-floating' key='wifi' onClick={() => this.changePage(PAGES.DEFAULT)}>
+        <span>Wifi</span>
+      </div>
+    )
+  }
+
+  renderImages () {
+    return (
+      <div className='full-screen-floating' key='images' onClick={() => this.changePage(PAGES.DEFAULT)}>
+        <span>Images</span>
       </div>
     )
   }
@@ -59,6 +134,10 @@ class Menu extends React.Component {
         {info}
       </div>
     )
+  }
+
+  changePage (page) {
+    this.setState({page: page})
   }
 
   @autobind
