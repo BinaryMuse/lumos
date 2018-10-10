e4b9c69393b2feea0215c4a1a801465545b3e226
Michelle Tilley <michelle@michelletilley.net>
2018-10-10 15:57:18 -0700

    Add content page

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,18 @@
+import path from 'path'
 import { app, BrowserWindow } from 'electron'
+
+const MAIN_WINDOW_CONTENT_URL = path.resolve(__dirname, '..', 'renderer', 'index.html')
 
 let mainWindow // eslint-disable-line no-unused-vars
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 400,
-    height: 200
+    title: 'Lumos',
+    width: 800,
+    height: 480,
+    show: true,
+    useContentSize: true
   })
+  mainWindow.loadURL(`file://${MAIN_WINDOW_CONTENT_URL}`)
 }
 
 app.on('ready', () => {
