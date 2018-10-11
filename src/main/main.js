2bedba030dbf346096fc5093910081b3ffad5cb6
Michelle Tilley <michelle@michelletilley.net>
2018-10-10 17:51:13 -0700

    Add main boot sequence and image protocol

--- src/main/main.js
+++ src/main/main.js
@@ -1,7 +1,18 @@
+import fs from 'fs'
+import os from 'os'
 import path from 'path'
-import { app, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow } from 'electron'
 
 const MAIN_WINDOW_CONTENT_URL = path.resolve(__dirname, '..', 'renderer', 'index.html')
+const LUMOS_IMAGE_DIR = process.env.LUMOS_IMAGE_DIR || path.join(os.homedir(), '.lumos_images')
+
+if (!fs.existsSync(LUMOS_IMAGE_DIR)) {
+  console.error(
+    `Lumos image directory ${LUMOS_IMAGE_DIR} does not exist on disk. ` +
+    `Create it or specify a different directory with the LUMOS_IMAGE_DIR environment variable`
+  )
+  app.exit(1)
+}
 
 let mainWindow // eslint-disable-line no-unused-vars
 function createWindow () {
@@ -9,12 +20,30 @@ function createWindow () {
     title: 'Lumos',
     width: 800,
     height: 480,
-    show: true,
+    show: false,
     useContentSize: true
   })
   mainWindow.loadURL(`file://${MAIN_WINDOW_CONTENT_URL}`)
+
+  // The app will not boot until it gets the `main-window-shown` event
+  mainWindow.once('show', () => {
+    mainWindow.webContents.send('boot-app')
+  })
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+}
+
+function registerImageProtocol () {
+  protocol.registerFileProtocol('lumos', (request, callback) => {
+    const relPath = request.url.replace(/^lumos:\/\//, '')
+    const imagePath = path.join(LUMOS_IMAGE_DIR, relPath)
+    callback(imagePath)
+  })
 }
 
 app.on('ready', () => {
+  registerImageProtocol()
   createWindow()
 })
--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,12 +1,32 @@
+import { ipcRenderer } from 'electron'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Spring } from 'react-spring'
 
 class App extends React.Component {
   render () {
     return (
-      <div>Hello from React!</div>
+      <Spring config={{ delay: 500, tension: 100, friction: 50 }} from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {styles => (
+          <div style={{ ...styles, color: 'white', flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <h1 style={{ textAlign: 'center' }}>L U M O S</h1>
+          </div>
+        )}
+      </Spring>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const render = () => ReactDOM.render(<App />, document.getElementById('app'))
+
+if (document.location.hash.endsWith('booted')) {
+  render()
+} else {
+  ipcRenderer.once('boot-app', () => {
+    // Set a flag so that when the page is refreshed we don't wait
+    // for the main process to instruct us to boot.
+    document.location.hash = 'booted'
+    render()
+  })
+}
