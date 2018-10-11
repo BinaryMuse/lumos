e153ca80adfab2227a3cbca50846f8b2b61d64a9
Michelle Tilley <michelle@michelletilley.net>
2018-10-10 20:52:51 -0700

    Slideshow experiment working

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -2,10 +2,61 @@ import { ipcRenderer } from 'electron'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Spring } from 'react-spring'
+import { Spring, Transition, config } from 'react-spring'
+
+const MAX_RANDOM = 1000000
+function random (max) {
+  return Math.floor(Math.random() * max)
+}
 
 class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      images: [
+        `https://placem.at/people?w=800&h=480&random=${random(MAX_RANDOM)}&txt=`,
+        `https://placem.at/people?w=800&h=480&random=${random(MAX_RANDOM)}&txt=`,
+        `https://placem.at/people?w=800&h=480&random=${random(MAX_RANDOM)}&txt=`
+      ]
+    }
+  }
+
+  componentDidMount () {
+    setInterval(() => {
+      this.setState((state) => {
+        const rest = state.images.slice(1)
+        rest.push(`https://placem.at/people?w=800&h=480&random=${random(MAX_RANDOM)}&txt=`)
+        return { images: rest }
+      })
+    }, 6000)
+  }
+
   render () {
+    const containerStyle = {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
+    }
+
+    const images = this.state.images.slice(0, 2)
+    return (
+      <div style={containerStyle}>
+        <Transition
+          keys={images}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+          config={config.slow}
+        >
+          {images.slice(0, 2).map(item => props => <div style={{ ...containerStyle, ...props, background: `url(${item})`, backgroundSize: 'cover' }} />)}
+        </Transition>
+        <img src={this.state.images[2]} style={{ position: 'absolute', left: -10000, top: -10000 }} />
+      </div>
+    )
+  }
+  renderOld () {
     return (
       <Spring config={{ delay: 500, tension: 100, friction: 50 }} from={{ opacity: 0 }} to={{ opacity: 1 }}>
         {styles => (
